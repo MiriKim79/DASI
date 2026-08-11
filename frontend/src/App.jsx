7fb8cbd42ecccdf1738bd08996537ee5a7618d74
junghocomputer <junghocomputer@kyonggi.ac.kr>
2026-08-12 02:05:05 +0900

    Feat: 회원가입 화면 및 입력 검증 구현 (#38)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceholderPage from "./pages/PlaceholderPage.jsx";
 import AgeTestStartPage from "./pages/AgeTest/StartPage.jsx";
 import AgeTestQuizPage from "./pages/AgeTest/QuizPage.jsx";
 import AgeTestResultPage from "./pages/AgeTest/ResultPage.jsx";
+import SignupPage from "./pages/Auth/SignupPage.jsx";
 import ChatbotFab from "./components/Chatbot/ChatbotFab.jsx";
 
 // 전체 앱 라우팅. 공통 Layout(사이드바+상단바) 아래에 각 화면이 들어간다.
@@ -40,6 +41,7 @@ export default function App() {
             path="/login"
             element={<PlaceholderPage title="로그인" note="회원/로그인 담당 팀원이 구현할 화면이에요. 🔒" />}
           />
+          <Route path="/signup" element={<SignupPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
